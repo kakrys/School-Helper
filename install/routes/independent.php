e0e52ada437d958e80bf4b829cae65fbdade16c9
Vadim Valeev <vadyapple@gmail.com>
2024-04-05 10:36:18 +0200

    added login+register layout

--- install/routes/independent.php
+++ install/routes/independent.php
@@ -11,6 +11,9 @@ return function (RoutingConfigurator $routes) {
 	#$routes->post('/task/delete', [\Up\Tasks\Controller\TaskEditor::class, 'delete']); оставлено как пример
 	$routes->get('/teacher', new PublicPageController('/local/modules/proj.independent/views/teacher.php'));
 	$routes->get('/student', new PublicPageController('/local/modules/proj.independent/views/student.php'));
+	$routes->get('/login', new PublicPageController('/local/modules/proj.independent/views/login.php'));
+	$routes->get('/register', new PublicPageController('/local/modules/proj.independent/views/register.php'));
+
 
 	$routes->any('/{route}',new PublicPageController('/local/modules/proj.independent/views/404.php'));
 };
---
+++ views/register.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @var CMain $APPLICATION
+ */
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Independent");
+?>
+
+	<section class="hero is-success is-fullheight">
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<div class="column is-4 is-offset-4">
+					<h3 class="title has-text-black">Login</h3>
+					<hr class="login-hr">
+					<p class="subtitle has-text-black">Please login to proceed.</p>
+					<div class="box">
+						<form>
+							<div class="field">
+								<div class="control">
+									<input class="input is-large" type="email" placeholder="Your Email" autofocus="">
+								</div>
+							</div>
+
+							<div class="field">
+								<div class="control">
+									<input class="input is-large" type="password" placeholder="Your Password">
+								</div>
+							</div>
+
+
+							<button class="button is-block is-info is-large is-fullwidth">Register <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+						</form>
+					</div>
+					<p class="has-text-grey">
+						<a href="/">Главная страница</a> &nbsp;·&nbsp;
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<script async type="text/javascript" src="../js/bulma.js"></script>
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
