f4ee9f8b513e2a365d653d9e81469f7ebdc8620a
Михаил Бакиновский <wolfsector207@gmail.com>
2024-04-09 21:05:27 +0200

    Some html-code refactor

--- install/templates/independent/header.php
+++ install/templates/independent/header.php
@@ -14,6 +14,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<title><?php $APPLICATION->ShowTitle(); ?></title>
+	<style>.bx-auth-reg{display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 1%;}</style>
 	<?php
 	$APPLICATION->ShowHead();
 	?>
