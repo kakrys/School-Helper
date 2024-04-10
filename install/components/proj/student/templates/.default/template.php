5ec72aca217552ae5dcb2c5a9eda538947480401
Vadim Valeev <vadyapple@gmail.com>
2024-04-10 13:45:50 +0200

    Added student account page

--- install/components/proj/student/templates/.default/template.php
+++ install/components/proj/student/templates/.default/template.php
@@ -6,70 +6,100 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 {
 	die();
 }
-#require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Independent");
+global $USER;
 ?>
-	<div class="columns">
-		<div class="navbar-left nav-menu box column">
-			<div class="container">
-				<span class="personal-information ">
-					Фамилия:Валеев
+<div class="container-fluid mt-1" style="margin-top: 1%; flex-grow: 1;">
+	<div class="main-content d-flex" style="width: 100%;">
+		<div class="d-flex flex-column" style="max-height: 83vh; min-width: 10%; max-width: 15%;">
+			<div class="border bg-light d-flex flex-column p-1" style="overflow-y: auto; overflow-x: auto;">
+				<span class="border border-3 text-primary">
+				<p style="align-self: center;">Фамилия: <?=$USER->GetLastName()?></p>
+				<p style="align-self: center;">Имя: <?=$USER->GetFirstName()?></p>
+				<p style="align-self: center;">Логин: <?=$USER->GetLogin()?></p>
+				<p style="align-self: center;">Роль: Ученик</p>
 				</span>
-						<span class="personal-information">
-					Имя:Вадим
-				</span>
-						<span class="personal-information">
-					Школа:129
-				</span>
-						<span class="personal-information">
-					Город:Челябинск
-				</span>
-						<span class="personal-information">
-					Роль:Учитель
-				</span>
+				<a href ='#' class="btn align-self-center" data-target="#changeData"> Смена личной информации</a>
+				<a href ='#' class="btn align-self-center" data-target="#statistics"> Статистика</a>
 			</div>
-			<div class="menu-buttons">
-				<ul>
-					<li class="is-active"><a class="button is-link mt-4" href="#teachers">Учителя</a></li>
-					<li><a class="button is-link" href="#tasks">Задания</a></li>
-					<li><a class="button is-link" href="#statistics">Статистика</a></li>
-				</ul>
-			</div>
+			<div style="flex-grow: 1;"></div>
 		</div>
-
-		<!--	ТУТ ВМЕСТО ТЕГОВ h2 НУЖНО НАПОЛНИТЬ ВКЛАДКИ КОНТЕНТОМ-->
-		<div class="tabs-content">
-			<div id="teachers" class="active">
-				<h2>Учителя</h2>
-			</div>
-			<div id="tasks">
-				<h2>Отправленные задания</h2>
-			</div>
-			<div id="statistics" >
-				<h2>Статистика ответов</h2>
+		<div style="min-height: 83vh; flex-grow: 1;">
+			<div class="d-flex flex-column" style="margin-left: 1%;">
+				<div class="border bg-light">
+							<div id="changeData" class="collapse">
+<!--								МЕСТО ДЛЯ ИЗМЕНЕНИЯ ВКЛАДКИ СМЕНА ЛИЧНЫХ ДАННЫХ-->
+								<div class="d-flex flex-column">
+									<form style="margin-bottom: 10px">
+										<div class="mb-3">
+											<label for="exampleInputNewLastName" class="form-label">Фамилия</label>
+											<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="LastNameHelp">
+											<div id="LastNameHelp" class="form-text">We'll never share your email with anyone else.</div>
+										</div>
+										<button type="submit" class="btn btn-primary">Изменить</button>
+									</form>
+									<form style="margin-bottom: 10px">
+										<div class="mb-3">
+											<label for="exampleInputNewLastName" class="form-label">Имя</label>
+											<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="LastNameHelp">
+											<div id="LastNameHelp" class="form-text">We'll never share your email with anyone else.</div>
+										</div>
+										<button type="submit" class="btn btn-primary">Изменить</button>
+									</form >
+									<form style="margin-bottom: 10px">
+										<div class="mb-3">
+											<label for="exampleInputNewLastName" class="form-label">Логин</label>
+											<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="LastNameHelp">
+											<div id="LastNameHelp" class="form-text">We'll never share your email with anyone else.</div>
+										</div>
+										<button type="submit" class="btn btn-primary">Изменить</button>
+									</form>
+									<form>
+										<div class="mb-3">
+											<label for="exampleInputNewLastName" class="form-label">Пароль</label>
+											<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="LastNameHelp">
+											<div id="LastNameHelp" class="form-text">We'll never share your email with anyone else.</div>
+										</div>
+										<button type="submit" class="btn btn-primary">Изменить</button>
+									</form>
+								</div>
+							</div>
+					<div id="statistics" class="collapse">
+<!--						МЕСТО ДЛЯ ИЗМЕНЕНИЯ ВКЛАДКИ 'СТАТИСТИКА'-->
+						<div class="d-flex flex-column">
+							<strong>coming soon</strong>
+							<div class="spinner-border text-info" role="status">
+								<span class="visually-hidden">Загрузка...</span>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<script>
-		window.addEventListener('load', () => {
-			const tabButtons = document.querySelectorAll('.menu-buttons a');
-			const tabContainers = document.querySelectorAll('.tabs-content div');
-
-			tabButtons.forEach(button => {
-				button.addEventListener('click', (event) => {
-					event.preventDefault();
-					tabButtons.forEach(btn => {
-						btn.parentElement.classList.remove('is-active');
-					});
-					button.parentElement.classList.add('is-active');
-
-					const id = event.target.hash.substring(1);
-					tabContainers.forEach(container => container.classList.remove('active'));
-					document.getElementById(id).classList.add('active');
-				})
-			})
-		})
-	</script>
-<?php
-#require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+<script>
+	const links = document.querySelectorAll('a[data-target]');
+	links.forEach(link => {
+		link.addEventListener('click', (event) => {
+			event.preventDefault();
+			const target = link.getAttribute('data-target');
+			const content = document.querySelector(target);
+			const showElements = document.querySelectorAll('.show');
+			if (content) {
+				const allContents = document.querySelectorAll('.collapse');
+				allContents.forEach(c => {
+					if (c !== content) {
+						c.classList.remove('show');
+					}
+				});
+				content.classList.toggle('show');
+				if (showElements.length === 0) {
+					const specificElement = document.getElementById('theme-1');
+					specificElement.classList.add('show');
+				}
+			}
+		});
+	});
+</script>
