dbc9fb250a2c69fdc2923159f33df4ce1d7a35ee
Михаил Бакиновский <wolfsector207@gmail.com>
2024-04-15 05:06:02 +0200

    New component (still bad)

---
+++ install/components/proj/exercises/class.php
@@ -0,0 +1,19 @@
+<?php
+
+class MainMenuComponent extends CBitrixComponent
+{
+	public function executeComponent()
+	{
+		$this->fetchExercises();
+		$this->includeComponentTemplate();
+	}
+
+	protected function fetchExercises()
+	{
+		$result = \Proj\Independent\Model\ExerciseTable::getList(['select' => [
+			'EXERCISE_DESCRIPTION','EXERCISE_CONDITION', 'EXERCISE_GENERATOR_RULES','GENERATOR_CODE', 'ANSWER', 'EXERCISE_ADDITION_FILE_PATH']]);
+		$selectionResult = $result->fetchAll();
+		$this->arResult['EXERCISES'] = $selectionResult;
+	}
+
+}
