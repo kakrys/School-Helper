13d5ed3e82a94aa4a393dd7c00f04e5980c8fe53
Михаил Бакиновский <wolfsector207@gmail.com>
2024-05-08 04:30:40 +0200

    Last comment cleaning

--- install/components/proj/exercises/class.php
+++ install/components/proj/exercises/class.php
@@ -10,28 +10,20 @@ class Exercises extends CBitrixComponent
 
 	protected function fetchExercises()
 	{
-		file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "------------------ХОБА! ГЕНЕРИМ ВАРИАНТ--------------\n", FILE_APPEND);
 		$request = \Bitrix\Main\Context::getCurrent()->getRequest();
 		$generatorCode = $request->getQueryList()->toArray()['generator_code'];
-		#file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "Наш кодик:".$generatorCode."\n", FILE_APPEND);
 		$exercises = \Proj\Independent\Repository\ExercisesRepository::getExercisesByVariant($generatorCode);
-		#file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "А вот такие у нас задачки нынче:".print_r($exercises, true)."\n", FILE_APPEND);
 		$newExercises = [];
 		foreach ($exercises as $exercise)
 		{
 			if ($exercise['GENERATOR_CODE'] !== null)
 			{
-				file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "-------\n", FILE_APPEND);
 				$exerciseManager = unserialize(gzuncompress($exercise['EXERCISE_GENERATOR_RULES']));
 				$exercise['EXERCISE_CONDITION'] = $exerciseManager->constructExercise(abs(intval(hexdec($generatorCode))));
-				file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "ВОТ МЫ НАДЕЛАЛИ ДЕЛОВ".$exercise['EXERCISE_CONDITION']."\n", FILE_APPEND);
 				$exercise['ANSWER'] = $exerciseManager->solve();
-				file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "А такой типа ответ".print_r($exerciseManager->solve(), true)."\n", FILE_APPEND);
-				file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "А вот такие у нас задачки нынче:".print_r($exerciseManager, true)."\n", FILE_APPEND);
 			}
 			$newExercises[] = $exercise;
 		}
-		#file_put_contents($_SERVER["DOCUMENT_ROOT"]."/logFile.txt", "А вот и финал:".print_r($exercises, true)."\n", FILE_APPEND);
 		$this->arResult['EXERCISES'] = $newExercises;
 		$this->arResult['GENERATOR_CODE'] = $generatorCode;
 	}
